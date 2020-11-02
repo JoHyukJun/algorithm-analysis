89591862cf3188e2beda08ccea846febb370df89
Jo Hyuk Jun <computer@khu.ac.kr>
2020-11-02 21:44:28 +0900

    daily update

---
+++ source/NumberCardTwo.py
@@ -0,0 +1,33 @@
+'''
+
+    main.py
+
+    Created by Jo Hyuk Jun on 2020
+    Copyright © 2020 Jo Hyuk Jun. All rights reserved.
+
+'''
+
+
+import sys
+
+n = int(sys.stdin.readline())
+card = dict()
+
+temp = list(map(int, sys.stdin.readline().rstrip().split(' ')))
+
+for i in range(n):
+    if temp[i] in card:
+        card[temp[i]] += 1
+    else:
+        card.setdefault(temp[i], 0)
+
+m = int(sys.stdin.readline())
+
+ckr = list(map(int, sys.stdin.readline().rstrip().split(' ')))
+
+for i in range(m):
+    if ckr[i] in card:
+        print(card[ckr[i]], end=' ')
+    else:
+        print(0, end=' ')
+
