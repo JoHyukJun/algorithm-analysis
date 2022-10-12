471b1c0670940d8adac4dcd1988117f9b9bb0f80
42decibel <hyukzuny@gmail.com>
2022-10-12 09:47:57 +0900

    feat:daily update

---
+++ source/python/A로 B 만들기.py
@@ -0,0 +1,12 @@
+'''
+
+    main.py
+
+    Created by JO HYUK JUN on 2022
+    Copyright © 2022 JO HYUK JUN. All rights reserved.
+
+'''
+
+
+def solution(before, after):
+    return 1 if sorted(before) == sorted(after) else 0
