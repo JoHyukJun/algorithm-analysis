2682ae8bc6ad744c68e80b17aa17d5a9aa466e91
42decibel <hyukzuny@gmail.com>
2022-10-19 20:19:15 +0900

    fix: Modify code

---
+++ source/cpp/완주하지 못한 선수.cpp
@@ -0,0 +1,29 @@
+﻿//
+//	main.cpp
+//
+//	Copyright © 2022 Jo Hyuk Jun. All rights reserved.
+//
+
+
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(vector<string> participant, vector<string> completion) {
+    string answer = "";
+    
+    sort(participant.begin(), participant.end());
+    sort(completion.begin(), completion.end());
+    
+    for(int i=0;i<completion.size();i++)
+    {
+        if(participant[i] != completion[i])
+            return participant[i];
+    }
+    
+    answer = participant[participant.size() - 1];
+    
+    return answer;
+}
